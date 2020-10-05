8e70a382d237b886b2bdf6c4eb5ae3e455353a42
Soma Balázsi <bsoma05@gmail.com>
2020-10-05 19:30:07 +0200

    Initial mockups.

---
+++ TudoMario/Configuration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TudoMario
+{
+    class Configuration
+    {
+        public static Developer Dev { get; }
+
+        public enum Developer { Adam, Dani, Patrik, Soma }
+    }
+}
