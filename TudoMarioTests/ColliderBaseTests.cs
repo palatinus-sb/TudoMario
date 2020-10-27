24f24bb9007c4fedda0495dcb386b6cfef0ddde1
Soma Balázsi <bsoma05@gmail.com>
2020-10-27 16:42:27 +0100

    Created Test project

---
+++ TudoMarioTests/ColliderBaseTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TudoMarioTests
+{
+    [TestClass]
+    class ColliderBaseTests
+    {
+        [TestMethod]
+        public void TestIsCollidingWith()
+        {
+            Assert.Fail();
+        }
+    }
+}
