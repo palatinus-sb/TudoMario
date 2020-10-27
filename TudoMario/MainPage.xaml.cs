fb4d2bc38245eb31c3bac1d48d8474f0ea11d9b5
Paaaaaaaatrik <72405066+Paaaaaaaatrik@users.noreply.github.com>
2020-10-27 17:33:07 +0100

    UserControlHandling

--- TudoMario/MainPage.xaml.cs
+++ TudoMario/MainPage.xaml.cs
@@ -15,6 +15,9 @@ using Windows.UI.Xaml.Navigation;
 using TudoMario.Map;
 using TudoMario.Ui;
 using TudoMario.Rendering;
+using Windows.UI.Core;
+using Windows.Services.Store;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -37,7 +40,62 @@ namespace TudoMario
         public MainPage()
         {
             InitializeComponent();
-            new LogicController();
+
+            bool ControlLeftPushed = false;
+            bool ControlRightPushed = false;
+            bool ControlUpPushed = false;
+            bool ControlDownPushed = false;
+
+            LogicController logiccontroller = new LogicController();
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+
+            void CoreWindow_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs e)
+            {
+                string KeyPressed = e.VirtualKey.ToString();
+                if ("W" == KeyPressed || "Space" == KeyPressed)
+                {
+                    ControlUpPushed = true;
+                }
+                if ("A" == KeyPressed)
+                {
+                    ControlLeftPushed = true;
+                }
+                if ("S" == KeyPressed)
+                {
+                    ControlDownPushed = true;
+                }
+                if ("D" == KeyPressed)
+                {
+                    ControlRightPushed = true;
+                }
+                if (ControlLeftPushed && ControlRightPushed)
+                {
+                    ControlLeftPushed = false;
+                    ControlRightPushed = false;
+                }
+            }
+
+            void CoreWindow_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs e)
+            {
+                string KeyPressed = e.VirtualKey.ToString();
+                if ("W" == KeyPressed || "Space" == KeyPressed)
+                {
+                    ControlUpPushed = false;
+                }
+                if ("A" == KeyPressed)
+                {
+                    ControlLeftPushed = false;
+                }
+                if ("S" == KeyPressed)
+                {
+                    ControlDownPushed = false;
+                }
+                if ("D" == KeyPressed)
+                {
+                    ControlRightPushed = false;
+                }
+            }
 
             switch (Configuration.Dev)
             {
